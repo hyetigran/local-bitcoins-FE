aefa675c699ba7468fdc384ebe9dbda20df9fb9c
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-24 20:05:01 +0400

    add aux file as helper

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,17 +4,23 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const Toolbar = props => (
+const Toolbar = () => (
+  // Reminder to pass props as arg for function
   <header className={classes.Toolbar}>
-    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <DrawerToggle
+      // Uncomment after Layout mapStateToProps
+      // clicked={props.drawerToggleClicked}
+      />
     <div className={classes.Logo}>
       <Logo />
     </div>
     <nav className={classes.DesktopOnly}>
-      <NavigationItems isAuthenticated={props.isAuth} />
+      <NavigationItems
+      //  Uncomment after Auth.js, auth actions and auth reducer
+      //  isAuthenticated={props.isAuth}
+       />
     </nav>
   </header>
-)
-
+);
 
 export default Toolbar;
