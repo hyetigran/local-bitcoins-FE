f91013455671032547c8dfc991cf09d5ae56d240
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-25 12:04:28 +0400

    add button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Button.css';
+
+const Button = props => (
+  <button
+    className={[classes.Button, classes[props.btnType]].join(' ')}
+    disabled={props.disabled}
+    onClick={props.clicked}
+    >
+    {props.children}
+    </button>
+);
+
+export default Button;
