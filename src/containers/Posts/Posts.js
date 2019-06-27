38807b6c5e19000ffbfbf9262775d78d05844d01
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-27 11:37:05 +0400

    add landing banner to home page

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -6,22 +6,40 @@ import axios from "../../axios-posts";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../store/actions/index";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import classes from "./Posts.css";
+import Button from "../../components/UI/Button/Button";
 
 class Posts extends React.Component {
   componentDidMount() {
     this.props.onFetchPosts();
   }
+
+  clickHandler = () => {
+    this.props.history.push("/auth");
+  };
+
   render() {
-    // let posts = <Spinner />;
-    // if (!this.props.loading) {
-    //   posts = this.props.posts.map(post => <Post key={post.id} {...post} />);
-    // }
+    let posts = <Spinner />;
+    if (!this.props.loading) {
+      posts = this.props.posts.map(post => <Post key={post.id} {...post} />);
+    }
+
+    let landingBanner = null;
+    if (!this.props.isAuth) {
+      landingBanner = (
+        <div className={classes.LandingBanner}>
+          <h1>Buy and Sell bitcoins in Armenia</h1>
+          <h2>No Fees. No KYC. No Hassle.</h2>
+          <Button clicked={this.clickHandler} btnType="SignUp">
+            Sign Up
+          </Button>
+        </div>
+      );
+    }
     return (
-      // <div>{posts}</div>
       <div>
-        {this.props.posts.map(post => (
-          <Post key={post.id} />
-        ))}
+        {landingBanner}
+        {posts}
       </div>
     );
   }
@@ -30,7 +48,8 @@ class Posts extends React.Component {
 const mapStateToProps = state => {
   return {
     posts: state.post.posts,
-    loading: state.post.loading
+    loading: state.post.loading,
+    isAuth: state.auth.token !== null
   };
 };
 
