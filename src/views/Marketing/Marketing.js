47f2795061eaab674f6df3895767574bf16299ba
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-17 14:22:19 -0400

    marketing view top intro complete

--- src/views/Marketing/Marketing.js
+++ src/views/Marketing/Marketing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "antd";
 import "./Marketing.scss";
 import gatewayImg from "../../assets/marketing/gateway.webp";
 
@@ -7,14 +8,22 @@ const Marketing = props => {
   //check auth state - if logged in do not display 'create account' button
   return (
     <div className="top-intro">
-      <div className="intro-left">
-        <h1>The private Bitcoin Cash exchange</h1>
-        <p>Buy and sell Bitcoin Cash (BCH) with anyone -anonymously</p>
-        <Link to="/offers">Browse listings</Link>
-        <Link to="/signup">Create account</Link>
-      </div>
-      <div className="intro-right">
-        <img src={gatewayImg} alt="gateway" />
+      <div className="top-intro-container">
+        <div className="intro-left">
+          <h1>The private Bitcoin Cash exchange</h1>
+          <p>Buy and sell Bitcoin Cash (BCH) with anyone -anonymously</p>
+          <Link to="/offers">
+            <Button sise="large" type="primary">
+              Browse listings
+            </Button>
+          </Link>
+          <Link to="/signup">
+            <Button sise="large">Create account</Button>
+          </Link>
+        </div>
+        <div className="intro-right">
+          <img src={gatewayImg} alt="gateway" />
+        </div>
       </div>
     </div>
   );
