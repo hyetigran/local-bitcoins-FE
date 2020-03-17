ca76c63a3f415e57bf6a2fc1bd02d33f51804ef4
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-17 12:32:17 -0400

    formatted and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 
 import Navigation from "./components/Navigation/Navigation";
 import Marketing from "./views/Marketing/Marketing";
