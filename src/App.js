158487234a18900a55ac6a7f1161514d77e6909b
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-17 00:49:01 -0400

    added antd and scss packages and created empty nav file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
-
 import "./App.css";
 
+import Navigation from "./components/Navigation/Navigation";
+
+import { Layout } from "antd";
+
+const { Header, Footer, Content } = Layout;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout>
+        <Header>
+          <Navigation />
+        </Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.scss";
+
+const Navigation = props => {
+  return (
+    <>
+      <div>logo</div>
+      <div>login</div>
+      <div>signup</div>
+    </>
+  );
+};
+
+export default Navigation;
