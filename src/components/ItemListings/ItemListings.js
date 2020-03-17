c4465b8feb50febaa170646da16b5a7cdf75613d
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-17 16:10:44 -0400

    styled secondary nav in the offer views

---
+++ src/components/ItemListings/ItemListings.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ItemListings.scss";
+
+const ItemListings = props => {
+  return <div>ItemListings component</div>;
+};
+
+export default ItemListings;
--- src/views/Offers/Offers.js
+++ src/views/Offers/Offers.js
@@ -1,23 +1,42 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
+import { Tooltip } from "antd";
+
 import Search from "../../components/Search/Search";
 import OfferListings from "../../components/OfferListings/OfferListings";
+import ItemListings from "../../components/ItemListings/ItemListings";
 import "./Offers.scss";
 
 const Offers = props => {
   return (
     <div className="offers-container">
       <div className="secondary-nav">
-        <div>Explore</div>
-        <div>
-          <NavLink>Trade BCH</NavLink>
+        <div className="title">Explore</div>
+        <div className="tab-link">
+          <NavLink to="/offers/trade" activeClassName="is-active">
+            Trade BCH
+          </NavLink>
         </div>
-        <div>
-          <NavLink>Marketplace</NavLink>
+        <div className="tab-link">
+          <NavLink to="/offers/goods" activeClassName="is-active">
+            <Tooltip
+              placement="bottom"
+              title="Buy and sell goods using Bitcoin Cash (BCH)"
+            >
+              Marketplace
+            </Tooltip>
+          </NavLink>
         </div>
       </div>
       <Search />
-      <OfferListings />
+      <Route
+        path="/offers/trade"
+        render={props => <OfferListings {...props} />}
+      />
+      <Route
+        path="/offers/goods"
+        render={props => <ItemListings {...props} />}
+      />
     </div>
   );
 };
