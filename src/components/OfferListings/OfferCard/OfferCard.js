03d6d4384f8cf04b0fe5124486d87c96057f32e6
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-18 01:11:48 -0400

    create offer listings outline

---
+++ src/components/OfferListings/OfferCard/OfferCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./OfferCard.scss";
+
+const OfferCard = props => {
+  return <div className="listings-container">OfferCard component</div>;
+};
+
+export default OfferCard;
--- src/components/OfferListings/OfferListings.js
+++ src/components/OfferListings/OfferListings.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./OfferListings.scss";
 
 const OfferListings = props => {
-  return <div className="listings-container">OfferListings component</div>;
+  return (
+    <div className="listings-container">
+      <div>{}</div>
+      <div>list container</div>
+      <div>button here to see all offers</div>
+    </div>
+  );
 };
 
 export default OfferListings;
+
+// Render two different text
+//Buy BCH from these sellers
+//Sell BCH to these buyers
+
+// button: 1. re-renders the view
+// 2. sets 'I want to...' Buy or Sell
+//3. create endless scroll
+//4. hide Buy or Sell component
