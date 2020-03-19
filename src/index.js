4c9f777fda3727a06fa17543f2db311c4c63e6c7
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-19 03:28:45 -0400

    imported dependencies and setup in index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
+// redux
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
