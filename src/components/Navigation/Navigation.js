9aeabab299c7b3297db97068beb6f427efb4303d
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-03-22 02:32:50 -0400

    navigation bar with auth links styled

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,50 +1,93 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { LockFilled, NotificationOutlined } from "@ant-design/icons";
+import { NavLink, Link } from "react-router-dom";
+import {
+  LockFilled,
+  NotificationOutlined,
+  TeamOutlined,
+  SwapOutlined,
+  UserOutlined,
+  WalletOutlined,
+  BellFilled,
+  SearchOutlined
+} from "@ant-design/icons";
 import { Button } from "antd";
 
 import "./Navigation.scss";
 import fullLogo from "../../assets/fullLogo.png";
 
 const Navigation = props => {
-  console.log(props.isAuth);
+  // need state for active trades and active offers
+  // need state for notifications
   return (
     <nav className="nav-bar">
       <div className="logo-container">
         <img src={fullLogo} alt="logo" />
       </div>
-      <div className="nav-links">
-        {!props.isAuth ? (
-          <>
-            <NavLink to="/login">
-              <LockFilled />
-              Log in
-            </NavLink>
-            <NavLink to="/signup">
-              <Button size="large" type="primary">
-                Create account
-              </Button>
-            </NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to="/offers/trade">
-              <div>
-                <NotificationOutlined />
-                Explore
+
+      {!props.isAuth ? (
+        <div className="nav-links">
+          <NavLink to="/login">
+            <LockFilled />
+            Log in
+          </NavLink>
+          <NavLink to="/signup">
+            <Button size="large" type="primary">
+              Create account
+            </Button>
+          </NavLink>
+        </div>
+      ) : (
+        <div className="nav-auth">
+          <Link to="/offers/trade">
+            <div className="link-box">
+              <SearchOutlined />
+              <p className="main-text">Explore</p>
+            </div>
+          </Link>
+          <NavLink to="/my-trades">
+            <div className="link-box">
+              <SwapOutlined />
+              <div className="link-text">
+                <p className="main-text">Trades</p>
+                <p>0 active</p>
               </div>
-            </NavLink>
-            <NavLink to="/my-trades">Trades</NavLink>
-            <NavLink to="/my-offers">Offers</NavLink>
-            <NavLink to="/wallet">Wallet</NavLink>
-            <NavLink to="/referral-program">Referrals</NavLink>
-            <NavLink to="/offers/trade">Account</NavLink>
-            <NavLink to="/new-offer">
-              <Button>+ New Offer</Button>
-            </NavLink>
-          </>
-        )}
-      </div>
+            </div>
+          </NavLink>
+          <NavLink to="/my-offers">
+            <div className="link-box">
+              <NotificationOutlined />
+              <div className="link-text">
+                <p className="main-text">Offers</p>
+                <p>0 active</p>
+              </div>
+            </div>
+          </NavLink>
+          <NavLink to="/wallet">
+            <div className="link-box">
+              <WalletOutlined />
+              <p className="main-text">Wallet</p>
+            </div>
+          </NavLink>
+          <NavLink to="/referral-program">
+            <div className="link-box">
+              <TeamOutlined />
+              <p className="main-text">Referrals</p>
+            </div>
+          </NavLink>
+          <NavLink to="/my-account">
+            <div className="link-box">
+              <UserOutlined />
+              <p className="main-text">Account</p>
+            </div>
+          </NavLink>
+          <div className="notification">
+            <BellFilled />
+          </div>
+          <NavLink to="/new-offer">
+            <Button>+ New Offer</Button>
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
