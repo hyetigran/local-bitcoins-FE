8cbcd633bd5d9e813ef41bed6ef92a18726c04f6
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-12 23:21:27 -0400

    added offers container and map data method

--- src/views/MyOffers/MyOffers.js
+++ src/views/MyOffers/MyOffers.js
@@ -2,18 +2,42 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "antd";
 import "./MyOffers.scss";
+import { axiosWithAuth } from "../../helpers/axiosWithAuth";
+import MyOfferCard from "../../components/MyOfferCard/MyOfferCard";
 
 const MyOffers = (props) => {
   const [myOffers, setMyOffers] = useState([]);
   //fetchOffersById
+  useEffect(() => {
+    fetchMyOffers();
+  });
+
+  const fetchMyOffers = async () => {
+    const userId = localStorage.getItem("userId");
+    try {
+      const result = await axiosWithAuth().get(`/offers/${userId}`);
+      console.log(result);
+      setMyOffers(result.data);
+    } catch (error) {
+      //handle error fetching
+      console.log(error);
+    }
+  };
   return (
-    <div className="my-offers-container">
+    <div className="my-offers-main">
       <h1>My offers</h1>
       <p className="sub-text">
         Creating an offer will allow you to set your own rate and terms, and
         enjoy a 0.25% fee. People that respond to offers pay a 0.75% fee.{" "}
-        {myOffers.length === 0 && <span>You don't have any offers yet.</span>}
       </p>
+      {myOffers.length === 0 && <span>You don't have any offers yet.</span>}
+      {myOffers && (
+        <div className="my-offer-container">
+          {myOffers.map((offer) => (
+            <MyOfferCard offer={offer} />
+          ))}
+        </div>
+      )}
       <Link to="/new-offer">
         <Button>CREATE A NEW OFFER</Button>
       </Link>
