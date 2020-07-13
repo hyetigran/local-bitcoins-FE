1553373ad802f6510e4240bcc1c4be43da6fbedd
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-12 23:06:55 -0400

    styled button

--- src/views/MyOffers/MyOffers.js
+++ src/views/MyOffers/MyOffers.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "antd";
 import "./MyOffers.scss";
 
 const MyOffers = (props) => {
+  const [myOffers, setMyOffers] = useState([]);
+  //fetchOffersById
   return (
     <div className="my-offers-container">
       <h1>My offers</h1>
-      <p>
+      <p className="sub-text">
         Creating an offer will allow you to set your own rate and terms, and
         enjoy a 0.25% fee. People that respond to offers pay a 0.75% fee.{" "}
-        <span>You don't have any offers yet.</span>
+        {myOffers.length === 0 && <span>You don't have any offers yet.</span>}
       </p>
-      <Link to="/new-offer">CREATE A NEW OFFER</Link>
+      <Link to="/new-offer">
+        <Button>CREATE A NEW OFFER</Button>
+      </Link>
     </div>
   );
 };
