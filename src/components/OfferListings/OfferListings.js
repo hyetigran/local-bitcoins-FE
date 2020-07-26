58261656989418efd6fd222be2b19618ea2780e6
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-07-25 21:19:07 -0400

    listening for new offer on socket

--- src/components/OfferListings/OfferListings.js
+++ src/components/OfferListings/OfferListings.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import openSocket from "socket.io-client";
 
 import OfferList from "./OfferList/OfferList";
 import "./OfferListings.scss";
@@ -7,8 +8,8 @@ const initialState = {
   showBoth: true,
   showBuyOnly: false,
   showSellOnly: false,
-  buyData: { isBuy: true, tradePosts: [] },
-  sellData: { isBuy: false, tradePosts: [] },
+  buyData: [],
+  sellData: [],
 };
 
 const OfferListings = (props) => {
@@ -20,6 +21,20 @@ const OfferListings = (props) => {
   const [data, setData] = useState(initialState);
   //setData will be called when building out the search component.
   //will need to move state up or refactor to store.
+  useEffect(() => {
+    const socket = openSocket("http://localhost:8000");
+    socket.on("offers", (data) => {
+      if (data.action === "create") {
+        addOffer(data.offer);
+      }
+    });
+  });
+  const addOffer = (offer) => {
+    //check buy/sell in order to add to correct state slice
+    const prop = offer.buyBCH ? "buyData" : "sellData";
+
+    this.setData({ ...data, [prop]: [offer, ...data[prop]] });
+  };
 
   return (
     <div className="main-offer-container">
